3d6cec5bca075a30f44d9bac6192b1930dcbad33
sir.sv7 <anasghwela128@gmail.com>
2025-05-23 00:01:58 +0200

    Add files via upload

---
+++ precache-manifest.c2aec4fc40a11cd5501e1e5e05b27fd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "973e7c777b80ab37abadf156b6744f15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34fb3479b6bdb3c51b2f",
+    "url": "/static/css/main.9df5886e.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "34fb3479b6bdb3c51b2f",
+    "url": "/static/js/main.22f6460c.chunk.js"
+  },
+  {
+    "revision": "8a5626378c6138c21833",
+    "url": "/static/js/runtime~main.cef9ab07.js"
+  }
+]);
